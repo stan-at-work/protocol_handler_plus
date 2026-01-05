5da7104a732c9ebf80fa93085dfc7500b944bfeb
Dick Verweij <dick.verweij@afas.nl>
2026-01-05 10:13:03 +0100

    Fix typo in export statement for FFI

--- packages/protocol_handler_windows/lib/src/protocol_handler_helper.dart
+++ packages/protocol_handler_windows/lib/src/protocol_handler_helper.dart
@@ -1,3 +1,3 @@
 export 'protocol_handler_helper.dart'
-    if (dart.library.ffi) 'protocol_handler_helpter_ffi.dart'
+    if (dart.library.ffi) 'protocol_handler_helper_ffi.dart'
     if (dart.library.html) 'protocol_handler_helper_web.dart';
